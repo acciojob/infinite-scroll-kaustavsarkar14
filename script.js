7513920a8e5f4a31688c939049d334cc71e5a749
Kaustav Sarkar <93545845+kaustavsarkar14@users.noreply.github.com>
2023-09-24 16:16:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,12 @@
 //your code here!
-
+let i = 11
+const ol = document.getElementById('infi-list')
+ol.addEventListener('scroll',()=>{
+  if(ol.scrollTop+ol.clientHeight>=ol.scrollHeight-1){
+    ol.innerHTML = ol.innerHTML + `
+		<li>Item ${i}</li>
+		<li>Item ${i+1}</li>
+ `
+	  i+=2
+  }
+})
